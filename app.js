b601857e710fe7f67f3fe286e6e5f3ef0c3cc82e
Andrey Lushchick <andrew@lushchick.org>
2012-01-06 01:56:22 +0200

    Pulling port from PORT env variable

--- app.js
+++ app.js
@@ -30,6 +30,5 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
-
-app.listen(42217);
+app.listen(process.env.PORT);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
